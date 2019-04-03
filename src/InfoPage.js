5609e887ebe13755409d0438ea389ca9eec3abec
saidivyachennupati <42659823+saidivyachennupati@users.noreply.github.com>
2019-04-02 20:52:27 -0500

    added

---
+++ src/InfoPage.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import ChoroplethMap from './components/ChoroplethMap';
+
+class InfoPage extends Component {
+ 
+  render() {
+    return (
+      <div style={{
+        height:"100vh",
+        width: "100vw"
+      }}>
+       
+      </div>
+    );
+  }
+}
+
+export default InfoPage;
